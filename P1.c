70a101f2ab8523a7bb74dff905e1f6d676c0f30f
Soroosh-Bsl <soroosh.baselizadeh@gmail.com>
2016-12-12 16:54:03 +0330

    Is Prime

--- P1.c
+++ P1.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int isprime(int n)
+{
+	int i = ;
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n%i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+
 int main()
 {
-	printf("Hello World!);
+	int n = 0;
+	scanf("%d", &n);
+	printf("%d", isprime(n));
 }
